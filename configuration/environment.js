b2fe7590f6bd239c272ecaa22a8566c994d9e3a1
Vadym <vadim.stebakov.94@gmail.com>
2020-12-14 07:54:51 +0200

    remove comments, extras scss

--- configuration/environment.js
+++ configuration/environment.js
@@ -3,13 +3,8 @@ const ip = require('ip');
 
 module.exports = {
     paths: {
-        /* Path to source files directory */
         source: path.resolve(__dirname, '../src/'),
-
-        /* Path to built files directory */
         output: path.resolve(__dirname, '../dist/'),
-
-        /* Assets */
         images: path.resolve(__dirname, '../src/images/'),
         fonts: path.resolve(__dirname, '../src/fonts/'),
     },
@@ -18,10 +13,7 @@ module.exports = {
         port: 8000,
     },
     limits: {
-        /* Image files size in bytes. Below this value the image file will be served as DataURL (inline base64). */
         images: 8192,
-
-        /* Font files size in bytes. Below this value the font file will be served as DataURL (inline base64). */
         fonts: 8192,
     },
 };
