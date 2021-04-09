aece09bbf28fdb45ceec8efd316d45d96d4f94ce
Vadym <vadim.stebakov.94@gmail.com>
2021-04-09 20:52:10 +0300

    add helper function isEqual

--- src/scripts/helpers/utils.js
+++ src/scripts/helpers/utils.js
@@ -20,6 +20,56 @@ export const asyncEach = (array, cb, interval = 10) => {
     next();
 };
 
+const getType = obj => {
+    return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+};
+
+export const isEqual = (a, b) => {
+    const areArraysEqual = () => {
+        if (a.length !== b.length) {
+            return false;
+        }
+
+        for (let i = 0; i < a.length; i++) {
+            if (!isEqual(a[i], b[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+    const areObjectsEqual = () => {
+        if (Object.keys(a).length !== Object.keys(b).length) {
+            return false;
+        }
+
+        for (const key in a) {
+            if (Object.prototype.hasOwnProperty.call(a, key)) {
+                if (!isEqual(a[key], b[key])) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    };
+    const arePrimitivesEqual = () => a === b;
+    const type = getType(a);
+
+    if (type !== getType(b)) {
+        return false;
+    }
+
+    switch (type) {
+        case 'array':
+            return areArraysEqual();
+        case 'object':
+            return areObjectsEqual();
+        default:
+            return arePrimitivesEqual();
+    }
+};
+
 export const findDuplicate = (array, amount) =>
     array.filter((item, _, list) => list.filter(_item => _item === item).length >= amount);
 
